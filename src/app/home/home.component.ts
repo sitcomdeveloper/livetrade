2e9bf298f95746f1047e3cecbf6966bfdba624e1
Rohan Gupta <sitcomdeveloper43@gmail.com>
2020-09-23 00:45:15 +0530

    Rohan 23-09

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, OnInit, AfterViewInit, Renderer2, Inject  } from '@angular/core';
 import { Router } from '@angular/router';
 import { BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
 import { PopupsComponent } from './popups/popups.component';
 
+declare const TradingView: any;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit,AfterViewInit  {
   getLoginDetails: any;
   bindLoginData: any;
-
-  constructor(private router:Router,private modalService: BsModalService) { }
+// this.Home: any;
+  constructor(private router:Router,private modalService: BsModalService, private renderer2: Renderer2, @Inject(DOCUMENT) private _document) { }
   bsModalRef: BsModalRef;
 
   ngOnInit(): void {
@@ -21,10 +23,42 @@ export class HomeComponent implements OnInit {
     this.getLoginDetails = JSON.parse(localStorage.getItem('project'));
     this.bindLoginData = this.getLoginDetails;
   }
+  ngAfterViewInit() {
+   
+    const s = this.renderer2.createElement('script');
+    s.type = 'text/javascript';
+    s.src = 'https://s3.tradingview.com/external-embedding/embed-widget-market-overview.js';
+    s.text = ``;
+    this.renderer2.appendChild(this._document.body, s);
+
+    // const t = this.renderer2.createElement('script');
+    // t.type = 'text/javascript';
+    // t.src = 'https://s3.tradingview.com/tv.js';
+    // t.text = ``;
+    // this.renderer2.appendChild(this._document.body, t);
+    // new TradingView.widget(
+    //   {
+    //   "width": 980,
+    //   "height": 610,
+    //   "symbol": "NASDAQ:AAPL",
+    //   "interval": "D",
+    //   "timezone": "Etc/UTC",
+    //   "theme": "light",
+    //   "style": "1",
+    //   "locale": "uk",
+    //   "toolbar_bg": "#f1f3f6",
+    //   "enable_publishing": false,
+    //   "allow_symbol_change": true,
+    //   "container_id": "tradingview_ddbbe"
+    //   }
+    //   );
+    
+  }
   // logout
   logout() {
     localStorage.clear();
-    this.router.navigateByUrl('/login');
+    this.router.navigateByUrl('/login'); 
+    window.location.href = "/login";
   }
   // deposit
   openpopupfordeposit() {
